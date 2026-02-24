2878068923e71b1a1c7e4ece4a5e5118a8332b6d
Nicolás <TheNicoVM7@gmail.com>
2026-02-23 23:50:49 -0300

    Se cambia idioma a admin de en a es

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.site_header = "Mi Portafolio"
+admin.site.site_title = "Portal de Administración"
+admin.site.index_title = "Bienvenido al Gestor de Contenido"
